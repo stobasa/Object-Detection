d08764c6b1a40ffd638cc04068e4aba519a20fc5
Obasa Samuel Temitope <stobasa96@gmail.com>
2019-04-12 15:31:58 +0100

    .

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def hello_world():
     return render_template('index.html')
@@ -16,3 +18,7 @@ def upload_file():
     file.save(f)
 
     return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
