f0d65ec8eb87deb557f13bdb217438e8ebd481db
Obasa Samuel Temitope <stobasa96@gmail.com>
2019-04-12 15:15:45 +0100

    .

---
+++ analyse.py
@@ -0,0 +1,17 @@
+import io
+from google.cloud import vision
+
+vision_client = vision.ImageAnnotatorClient()
+
+file_name=""
+
+
+def analyse(file_name):
+    with io.open(file_name,"rb") as image_file:
+        image = vision_client.annotate_image({'image': {'source': {'image_uri': image_file}},
+                                              'features': [{'type': vision.enums.Feature.Type.LABEL_DETECTION}], })
+
+    labels = image.detect_labels()
+
+    for label in labels:
+        print(label.description)
